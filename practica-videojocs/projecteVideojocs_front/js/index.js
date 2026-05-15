1d5379759959e484a9b33fac0db77de706e84dd3
RdDoogie <jsastre128@alumnes.politecnicllevant.cat>
2026-05-15 08:59:03 +0200

    Final commit amb els fixes finals

--- practica-videojocs/projecteVideojocs_front/js/index.js
+++
@@ -1,7 +0,0 @@
-async function ferProva() {
-    console.log("Connectant amb el servidor...");
-    const jocs = await consultar("SELECT * FROM videojocs LIMIT 5"); 
-    console.log("Les dades rebudes són:", jocs);
-}
-
-ferProva();
